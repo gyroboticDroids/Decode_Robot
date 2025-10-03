2f149dbedde5c41fb8a890f263b342ef4baf325d
awhol <blueshots06@gmail.com>
2025-10-03 15:46:44 -0400

    Added vision to Hardware

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassamblies/Hardware.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/subassamblies/Hardware.java
@@ -1,4 +1,19 @@
 package org.firstinspires.ftc.teamcode.subassamblies;
 
+import com.qualcomm.hardware.limelightvision.Limelight3A;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+
 public class Hardware {
+
+    //vision
+    public Limelight3A limelight;
+
+    public Hardware(HardwareMap hardwareMap)
+    {
+        limelight = hardwareMap.get(Limelight3A.class, "limelight");
+        limelight.setPollRateHz(100);
+        limelight.start();
+
+        limelight.pipelineSwitch(0);
+    }
 }
