0fcf31398c9728534e8ad8042505733d417575a4
awhol <blueshots06@gmail.com>
2025-10-25 15:15:25 -0400

    Started the Intake class

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/IntakeConstants.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/constants/IntakeConstants.java
@@ -1,4 +1,12 @@
 package org.firstinspires.ftc.teamcode.robot.constants;
 
 public class IntakeConstants {
+    public static double INTAKE_PIVOT_LEFT_DOWN = 0;
+    public static double INTAKE_PIVOT_LEFT_UP = 0;
+    public static double INTAKE_PIVOT_RIGHT_DOWN = 0;
+    public static double INTAKE_PIVOT_RIGHT_UP = 0;
+    public static int INTAKE_FORWARD = 0;
+    public static int INTAKE_BACKWARD = 0;
+
+
 }
--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subassamblies/Intake.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/subassamblies/Intake.java
@@ -1,26 +1,46 @@
 package org.firstinspires.ftc.teamcode.robot.subassamblies;
 
-import com.pedropathing.util.Timer;
+import org.firstinspires.ftc.teamcode.robot.constants.IntakeConstants;
 
 public class Intake {
     public enum State{
-        INTAKE, CLEAR, INTAKEUP
+        INTAKE, CLEAR, INTAKE_UP
     }
 
     Hardware hardware;
 
     private State state;
-    private Timer timer;
 
     public Intake(Hardware hardware){
         this.hardware = hardware;
     }
 
     public void update(){
+        switch (state){
+            case INTAKE:
+                hardware.intakePivotLeft.setPosition(IntakeConstants.INTAKE_PIVOT_LEFT_DOWN);
+                hardware.intakePivotLeft.setPosition(IntakeConstants.INTAKE_PIVOT_RIGHT_DOWN);
 
+                hardware.intake.setPower(IntakeConstants.INTAKE_FORWARD);
+                break;
+
+            case CLEAR:
+                hardware.intakePivotLeft.setPosition(IntakeConstants.INTAKE_PIVOT_LEFT_DOWN);
+                hardware.intakePivotLeft.setPosition(IntakeConstants.INTAKE_PIVOT_RIGHT_DOWN);
+
+                hardware.intake.setPower(IntakeConstants.INTAKE_BACKWARD);
+                break;
+
+            case INTAKE_UP:
+                hardware.intakePivotLeft.setPosition(IntakeConstants.INTAKE_PIVOT_LEFT_UP);
+                hardware.intakePivotLeft.setPosition(IntakeConstants.INTAKE_PIVOT_RIGHT_UP);
+
+                hardware.intake.setPower(IntakeConstants.INTAKE_FORWARD);
+                break;
+        }
     }
 
-    public void setState(String s){
-
+    public void setState(State s){
+        state = s;
     }
 }
