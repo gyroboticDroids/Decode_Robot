62e4b556def9c7689f63c9429d89091d9327c50c
NaeNae4321 <debruler.nl@gmail.com>
2025-11-04 15:51:13 -0500

    worked on auto

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/FarAuto.java
@@ -0,0 +1,273 @@
+package org.firstinspires.ftc.teamcode.robot.auto;
+
+import com.pedropathing.follower.Follower;
+import com.pedropathing.geometry.BezierLine;
+import com.pedropathing.geometry.Pose;
+import com.pedropathing.paths.Path;
+import com.pedropathing.util.Timer;
+import com.qualcomm.robotcore.eventloop.opmode.OpMode;
+
+import org.firstinspires.ftc.teamcode.pedroPathing.Constants;
+import org.firstinspires.ftc.teamcode.robot.constants.TransferConstants;
+import org.firstinspires.ftc.teamcode.robot.subassamblies.Hardware;
+import org.firstinspires.ftc.teamcode.robot.subassamblies.Intake;
+import org.firstinspires.ftc.teamcode.robot.subassamblies.Shooter;
+
+import java.util.EnumMap;
+
+@com.qualcomm.robotcore.eventloop.opmode.Autonomous(name = "close auto", group = "auto", preselectTeleOp = "Master Tele-op")
+public class FarAuto extends OpMode {
+    private enum PoseName{
+        START, SCORE_PRELOAD, SPIKE_MARK_3, SCORE, HUMAN_PLAYER_SPIKE_MARK, GATE_BALLS
+    }
+
+    EnumMap<PoseName, Pose> poses = new EnumMap<>(PoseName.class);
+
+    private Path scorePreload, collectBalls1, scoreBalls1, collectBalls2, scoreBalls2, collectBalls3, scoreBalls3;
+
+    private Follower follower;
+    private Timer pathTimer;
+
+    private Hardware hardware;
+    private Intake intake;
+    private Shooter shooter;
+
+    private boolean ons = false;
+    private boolean robotAtEnd = false;
+
+    private int pathState = -1;
+    private boolean allianceColorRed = true;
+
+
+    @Override
+    public void init() {
+        setUpPoses();
+
+        pathTimer = new Timer();
+        follower = Constants.createFollower(hardwareMap);
+        follower.setStartingPose(poses.get(PoseName.START));
+
+        hardware = new Hardware(hardwareMap);
+        hardware.setPoseTrackerInAuto(follower.poseTracker);
+
+        intake = new Intake(hardware);
+        shooter = new Shooter(hardware);
+
+        intake.setState(Intake.State.INTAKE_SLEEP);
+        shooter.setState(Shooter.State.OFF);
+    }
+
+    @Override
+    public void init_loop() {
+        if (gamepad1.crossWasPressed())
+            allianceColorRed = !allianceColorRed;
+
+        telemetry.addLine("alliance " + (allianceColorRed ? "RED" : "BLUE"));
+        telemetry.update();
+    }
+
+    @Override
+    public void start() {
+        setPathState(0);
+
+        if (!allianceColorRed) {
+            mirrorPoses();
+        }
+
+        buildPaths();
+    }
+
+    @Override
+    public void loop() {
+        follower.update();
+        autonomousPathUpdate();
+
+        telemetry.addData("path state", pathState);
+        telemetry.addData("x", follower.getPose().getX());
+        telemetry.addData("y", follower.getPose().getY());
+        telemetry.addData("heading", Math.toDegrees(follower.getPose().getHeading()));
+        telemetry.addData("robot at end", robotAtEnd);
+        telemetry.update();
+    }
+
+    private void autonomousPathUpdate() {
+        robotAtEnd = follower.getCurrentTValue() > 0.98;
+
+        switch (pathState) {
+            case 0:
+                intake.setState(Intake.State.INTAKE);
+                shooter.setState(Shooter.State.READY);
+
+                follower.followPath(scorePreload);
+
+                setPathState(pathState + 1);
+                break;
+
+            case 1:
+                if (!shooter.isBusy() && robotAtEnd) {
+                    shooter.setState(Shooter.State.LAUNCH);
+
+                    setPathState(pathState + 1);
+                }
+                break;
+
+            case 2:
+                if (!shooter.isBusy()) {
+                    shooter.setState(Shooter.State.READY);
+                    follower.followPath(collectBalls1);
+
+                    setPathState(pathState + 1);
+                }
+                break;
+
+            case 3:
+                if (robotAtEnd || !ons) {
+                    if (ons) {
+                        pathTimer.resetTimer();
+                        ons = false;
+                    }
+
+                    if (pathTimer.getElapsedTimeSeconds() > 1) {
+                        follower.followPath(scoreBalls1);
+                        setPathState(pathState + 1);
+                    }
+                }
+                break;
+
+            case 4:
+                if (robotAtEnd) {
+                    shooter.setState(Shooter.State.LAUNCH);
+
+                    setPathState(pathState + 1);
+                }
+                break;
+
+            case 5:
+                if (robotAtEnd || !ons) {
+                    if (ons) {
+                        pathTimer.resetTimer();
+                        ons = false;
+                    }
+
+                    if (pathTimer.getElapsedTimeSeconds() > 1) {
+                        follower.followPath(collectBalls2);
+                        setPathState(pathState + 1);
+                    }
+                }
+                break;
+
+            case 6:
+                if (robotAtEnd || !ons) {
+                    if (ons) {
+                        pathTimer.resetTimer();
+                        ons = false;
+                    }
+
+                    if (pathTimer.getElapsedTimeSeconds() > 1) {
+                        follower.followPath(scoreBalls2);
+                        setPathState(pathState + 1);
+                    }
+                }
+                break;
+
+            case 7:
+                if (robotAtEnd) {
+                    shooter.setState(Shooter.State.LAUNCH);
+
+                    setPathState(pathState + 1);
+                }
+                break;
+
+            case 8:
+                if (!shooter.isBusy()) {
+                    shooter.setState(Shooter.State.READY);
+                    follower.followPath(collectBalls3);
+
+                    setPathState(pathState + 1);
+                }
+                break;
+
+            case 9:
+                if (robotAtEnd || !ons) {
+                    if (ons) {
+                        pathTimer.resetTimer();
+                        ons = false;
+                    }
+
+                    if (pathTimer.getElapsedTimeSeconds() > 1) {
+                        follower.followPath(scoreBalls3);
+                        setPathState(pathState + 1);
+                    }
+                }
+                break;
+
+            case 10:
+                if (robotAtEnd) {
+                    shooter.setState(Shooter.State.LAUNCH);
+
+                    setPathState(pathState + 1);
+                }
+                break;
+
+            case 11:
+                if (!shooter.isBusy()) {
+                    shooter.setState(Shooter.State.READY);
+                    follower.followPath(collectBalls2);
+
+                    setPathState(-1);
+                }
+                break;
+        }
+    }
+
+    private void setUpPoses() {
+        poses.put(PoseName.START, new Pose(0, 0, Math.toRadians(90)));
+        poses.put(PoseName.SCORE_PRELOAD, new Pose(0, 0, Math.toRadians(90)));
+        poses.put(PoseName.SPIKE_MARK_3, new Pose(0, 0, Math.toRadians(90)));
+        poses.put(PoseName.SCORE, new Pose(0, 0, Math.toRadians(90)));
+        poses.put(PoseName.HUMAN_PLAYER_SPIKE_MARK, new Pose(0, 0, Math.toRadians(90)));
+        poses.put(PoseName.GATE_BALLS, new Pose(0, 0, Math.toRadians(90)));
+    }
+
+    private void buildPaths() {
+        scorePreload = new Path(new BezierLine(poses.get(PoseName.START), poses.get(PoseName.SCORE_PRELOAD)));
+        scorePreload.setConstantHeadingInterpolation(90);
+
+        collectBalls1 = new Path(new BezierLine(poses.get(PoseName.SCORE_PRELOAD), poses.get(PoseName.HUMAN_PLAYER_SPIKE_MARK)));
+        collectBalls1.setConstantHeadingInterpolation(90);
+
+        scoreBalls1 = new Path(new BezierLine(poses.get(PoseName.HUMAN_PLAYER_SPIKE_MARK), poses.get(PoseName.SCORE)));
+        scoreBalls1.setConstantHeadingInterpolation(90);
+
+        collectBalls2 = new Path(new BezierLine(poses.get(PoseName.SCORE), poses.get(PoseName.SPIKE_MARK_3)));
+        collectBalls2.setConstantHeadingInterpolation(90);
+
+        scoreBalls2 = new Path(new BezierLine(poses.get(PoseName.SPIKE_MARK_3), poses.get(PoseName.SCORE)));
+        scoreBalls2.setConstantHeadingInterpolation(90);
+
+        collectBalls3 = new Path(new BezierLine(poses.get(PoseName.SCORE), poses.get(PoseName.GATE_BALLS)));
+        collectBalls3.setConstantHeadingInterpolation(90);
+
+        scoreBalls3 = new Path(new BezierLine(poses.get(PoseName.GATE_BALLS), poses.get(PoseName.SCORE)));
+        scoreBalls3.setConstantHeadingInterpolation(90);
+    }
+
+    private void mirrorPoses() {
+        for (Pose pose : poses.values()) {
+            pose.mirror();
+        }
+    }
+
+    public void setPathState(int p) {
+        pathState = p;
+        ons = true;
+        pathTimer.resetTimer();
+    }
+
+    @Override
+    public void stop() {
+        TransferConstants.endPose = follower.getPose();
+        TransferConstants.isAllianceColorRed = allianceColorRed;
+        TransferConstants.endTurretPos = hardware.turret.getCurrentPosition();
+    }
+}
