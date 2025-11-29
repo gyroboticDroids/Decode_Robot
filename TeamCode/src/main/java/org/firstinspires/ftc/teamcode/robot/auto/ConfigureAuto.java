7004b241b556e1ab764067eb3f44ef1b7bdb9980
awhol <blueshots06@gmail.com>
2025-11-29 17:05:41 -0500

    changed auto

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/ConfigureAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/auto/ConfigureAuto.java
@@ -10,13 +10,15 @@ import java.io.File;
 import java.util.ArrayList;
 import java.util.List;
 
-@Autonomous(name = "configure far auto", group = "far auto")
-public class ConfigureFarAuto extends OpMode {
+@Autonomous(name = "configure auto", group = "far auto")
+public class ConfigureAuto extends OpMode {
     private List<Integer> routine;
     private final String[] names = {"spike mark 1", "spike mark 2", "spike mark 3", "gate", "human player", "wait (1 second)"};
+    private final String[] scoreNames = {"score close","score middle","score far"};
     private final double[] times = {4, 4.5, 5, 5, 1};
 
     private int selectedTask = 0;
+    private int selectedScorePosition = 0;
     private boolean dpadOns = false;
     private boolean prevGpadSquare = false;
     private boolean manageTaskOns = false;
@@ -33,17 +35,18 @@ public class ConfigureFarAuto extends OpMode {
                 "\ntriangle to delete all tasks \nsquare to toggle shooting area");
 
         selectedTask += (gamepad1.dpad_up && !dpadOns ? 1 : 0) - (gamepad1.dpad_down && !dpadOns ? 1 : 0);
+        selectedScorePosition += (gamepad1.dpad_right && !dpadOns ? 1 : 0) - (gamepad1.dpad_left && !dpadOns ? 1 : 0);
 
-        if(selectedTask > names.length - 1)
-            selectedTask = 0;
-        else if(selectedTask < 0)
-            selectedTask = names.length - 1;
+        selectedTask = Math.floorMod(selectedTask, 2);
+        selectedScorePosition = Math.floorMod(selectedScorePosition, 3);
 
         telemetry.addLine("next task = " + names[selectedTask]);
+        telemetry.addLine("score position = " + scoreNames[selectedScorePosition]);
 
         if(!manageTaskOns) {
             if (gamepad1.cross) {
                 routine.add(selectedTask);
+                routine.add(selectedScorePosition);
             } else if (gamepad1.circle && routine.size() > 1) {
                 routine.remove(routine.size() - 1);
             } else if (gamepad1.triangle) {
@@ -77,7 +80,7 @@ public class ConfigureFarAuto extends OpMode {
 
         telemetry.update();
 
-        dpadOns = gamepad1.dpad_up || gamepad1.dpad_down;
+        dpadOns = gamepad1.dpad_up || gamepad1.dpad_down || gamepad1.dpad_left || gamepad1.dpad_right;
         manageTaskOns = gamepad1.cross || gamepad1.circle || gamepad1.triangle;
     }
 
